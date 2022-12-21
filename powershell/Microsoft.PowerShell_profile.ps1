ca9760a0bb6737154de6c1ab6b0fb824c17004cc
jtotty <jt.totty1991@gmail.com>
2022-12-21 20:37:30 +0000

    Added powershell settings

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+# oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config ~/Documents/PowerShell/themes/james.json | Invoke-Expression
+Import-Module -Name Terminal-Icons
