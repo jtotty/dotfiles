712b7790851f1638eb9f36332218ab7f083e3210
jtotty <jt.totty1991@gmail.com>
2024-02-14 07:29:08 +0000

    Added noice

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,12 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    event = "ColorScheme",
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "rose-pine",
-            },
-        })
-    end,
+	"nvim-lualine/lualine.nvim",
+	event = "ColorScheme",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "rose-pine",
+				globalstatus = true,
+			},
+		})
+	end,
 }
