6bc3488fc930ee25656cd9344e2454a0a0222dfa
jtotty <jt.totty1991@gmail.com>
2024-09-14 11:07:31 +0100

    Update lazygit theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,54 +1,24 @@
 return {
-    -- {
-    --     'folke/tokyonight.nvim',
-    --     lazy = false, -- make sure it's loaded during startup
-    --     priority = 1000, -- make sure to load before all the other start plugins
-    --     config = function()
-    --         require('tokyonight').setup {
-    --             style = 'night',
-    --             transparent = false,
-    --             dim_inactive = false,
-    --             styles = {
-    --                 sidesbars = 'dark',
-    --                 floats = 'dark',
-    --             },
-    --             sidebars = { 'neo-tree' },
-    --             lualine_bold = true,
-    --         }
-    --
-    --         vim.cmd.colorscheme 'tokyonight'
-    --     end,
-    -- },
     {
-        'scottmckendry/cyberdream.nvim',
-        lazy = false,
-        priority = 1000,
+        'folke/tokyonight.nvim',
+        lazy = false, -- make sure it's loaded during startup
+        priority = 1000, -- make sure to load before all the other start plugins
         config = function()
-            require('cyberdream').setup {
-                borderless_telescope = false,
-                extensions = {
-                    telescope = true,
-                    notify = true,
+            require('tokyonight').setup {
+                style = 'night',
+                transparent = false,
+                dim_inactive = false,
+                styles = {
+                    sidesbars = 'dark',
+                    floats = 'dark',
                 },
+                sidebars = { 'neo-tree' },
+                lualine_bold = true,
             }
 
-            vim.cmd.colorscheme 'cyberdream'
+            vim.cmd.colorscheme 'tokyonight'
         end,
     },
-    -- {
-    --     'AlexvZyl/nordic.nvim',
-    --     lazy = false,
-    --     priority = 1000,
-    --     config = function()
-    --         require('nordic').setup {
-    --             bold_keywords = true,
-    --             transparent_bg = true,
-    --         }
-    --
-    --         vim.cmd.colorscheme 'nordic'
-    --     end,
-    -- },
-    -- Highlight todo, notes, etc in comments
     {
         'folke/todo-comments.nvim',
         event = 'VimEnter',
